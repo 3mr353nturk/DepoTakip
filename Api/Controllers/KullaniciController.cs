9c2620f29420f3028acdf0cf4fed5ab845c1548a
3mr353nturk <42348554+3mr353nturk@users.noreply.github.com>
2020-02-04 23:05:08 +0300

    DepoTakip v2

---
+++ Api/Controllers/KullaniciController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace Api.Controllers
+{
+    public class KullaniciController : ApiController
+    {
+
+        public string KullaniciGetir()
+        {
+            return "merhaba";
+        }
+
+        /// <summary>
+        /// Parametre istek öneği
+        /// 
+        ///  {
+        ///    "KULLANICI_AD" : "",
+        ///    "SIFRE" : ""
+        ///  }
+        /// 
+        /// </summary>
+        /// <param name="model"></param>
+        /// <returns></returns>
+
+        public DenemeKullanici Login(DenemeKullanici model)
+        {
+            DenemeKullanici kul = new DenemeKullanici()
+            {
+                KULLANICI_AD = model.KULLANICI_AD,
+                SIFRE = model.SIFRE
+            };
+            return kul; 
+          
+        }
+        public class DenemeKullanici
+        {
+            public string KULLANICI_AD { get; set; }
+            public string SIFRE { get; set; }
+        }
+    }
+}
